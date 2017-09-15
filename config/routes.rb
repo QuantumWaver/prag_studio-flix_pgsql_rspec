c6fba7ab328c9ccd70f012473a327dc99bbb3656
QuantumWaver <sholl76@gmail.com>
2017-09-15 17:28:31 -0400

    Add Movies controller and view with Index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  #root to: 'movies#index' # root_path or root_url
+
+  # Routes for Movie model
+  # mapping requests (type and resource) to a controller/action pair
+  get 'movies', to: 'movies#index'
+  #get 'movies/:id', to: 'movies#show', as: 'movie'
+
+
 end
