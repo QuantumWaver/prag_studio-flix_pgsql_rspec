0972cae255d8c029309ecbf96fa0f32dfbbac5ce
QuantumWaver <sholl76@gmail.com>
2017-09-15 18:45:10 -0400

    Add links between movies index and show pages

---
+++ spec/features/navigate_movies_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Navigating movies" do
+
+  before do
+    @movie = Movie.create(movie_attributes)
+  end
+
+  it "allows navigation from the detail page to the listing page" do
+    visit movie_url(@movie)
+
+    click_link "All Movies"
+    expect(current_path).to eq(movies_path)
+  end
+
+  it "allows navigation from the listing page to the detail page" do
+      visit movies_url
+
+      click_link @movie.title
+      expect(current_path).to eq(movie_path(@movie))
+    end
+end
