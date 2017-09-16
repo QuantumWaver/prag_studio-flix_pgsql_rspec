9eda8cc81b2216dc07a6b015ddbaf6a32aa12a52
QuantumWaver <sholl76@gmail.com>
2017-09-16 15:42:04 -0400

    Make the movies update form submit and update correctly

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,4 +12,20 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    # this will throw an exception if ':movie' param is not present
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+
+    # this will return an empty hash if the ':movie' param is not present
+    #params.fetch(:movie, {}).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
 end
