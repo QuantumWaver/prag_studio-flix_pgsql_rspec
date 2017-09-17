014ad235f03e9c279fea6cdd1bf0edd11e904b4a
QuantumWaver <sholl76@gmail.com>
2017-09-17 13:02:51 -0400

    Add functionality to delete movies

---
+++ spec/features/delete_movie_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Deleting a movie" do
+
+  it "deletes the movie and redirects to index" do
+    movie = Movie.create(movie_attributes)
+
+    visit movie_url(movie)
+
+    click_link 'Delete'
+
+    expect(Movie.find_by(title: movie.title)).to be_nil
+    expect(current_path).to eq(movies_path)
+    expect(page).not_to have_text(movie.title)
+  end
+
+end
+
